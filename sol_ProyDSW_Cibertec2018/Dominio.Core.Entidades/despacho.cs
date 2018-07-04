9c0c673b249eba2af91f191cff42b63df610bb5d
suite-sj202 <suite-sj202@SUITESJ202-ST17>
2018-07-03 21:34:07 -0500

    clase despacho

---
+++ sol_ProyDSW_Cibertec2018/Dominio.Core.Entidades/despacho.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Core.Entidades
+{
+    class despacho
+    {
+        [DisplayName("Código de Despacho")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Numero de despacho Obligatorio")]
+        public string nrodespacho { get; set; }
+
+        public DateTime fechatraslado { get; set; }
+
+        public DateTime fechaentrega { get; set; }
+        public string destino { get; set; }
+        public string transporte { get; set; }
+        public int estado { get; set; }
+
+
+
+    }
+}
