2c7542582239b075920356806ca40e08eccf8a24
suite-sj202 <suite-sj202@SUITESJ202-ST18>
2018-07-03 21:29:12 -0500

    Clase Det_bol y bol

---
+++ sol_ProyDSW_Cibertec2018/Dominio.Core.Entidades/Boleta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Core.Entidades
+{
+    public class Boleta
+    {
+        // NROBOL,CODCLI,CODEMP,FECHAEMICION,FECHAVENC,ESTADO
+        [DisplayName("Número de Boleta")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Número de Boleta")]
+        public string NROBOL { get; set; }
+
+        [DisplayName("Código de Cliente")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Código de Cliente")]
+        public string CODCLI { get; set; }
+
+        [DisplayName("Código de Empleado")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Código de Empleado")]
+        public string CODEMP { get; set; }
+
+        [DisplayName("Fecha de Emision")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fecha de Emision")]
+        public DateTime FECHAEMICION { get; set; }
+
+        [DisplayName("Fecha de Vencimiento")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Fecha de Vencimiento")]
+        public DateTime FECHAVENC { get; set; }
+
+        [DisplayName("Estado")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Estado")]
+        public int ESTADO { get; set; }
+    }
+}
---
+++ sol_ProyDSW_Cibertec2018/Dominio.Core.Entidades/Detalle_Boleta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Core.Entidades
+{
+    public class Detalle_Boleta
+    {
+        // NROBOL,CODPRO,CANTIDAD,PRECIO
+        [DisplayName("Número de Boleta")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Número de Boleta")]
+        public string NROBOL { get; set; }
+
+        [DisplayName("Código de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Código de Producto")]
+        public string CODPRO { get; set; }
+
+        [DisplayName("Cantidad a adquirir")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Cantidad a adquirir")]
+        public int CANTIDAD { get; set; }
+
+        [DisplayName("Precio del Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Precio del Producto")]
+        public double PRECIO { get; set; }
+    }
+}
