7e0193d0561255701cd687a8b2cdbdae29cd3de0
suite-sj201 <suite-sj201@SUITESJ201-ST17>
2018-07-03 19:01:17 -0500

    1.- Cambios Gallardo Mantenimiento Empleado

---
+++ sol_ProyDSW_Cibertec2018/Dominio.MainModule/EmpleadoManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Infraestructura.Data.SqlServer;
+using Dominio.Core.Entidades;
+
+namespace Dominio.MainModule
+{
+    public class EmpleadoManager
+    {
+
+        EmpleadoDAL objL = new EmpleadoDAL();
+
+        public List<Empleado> Listado_Todo_Emleado()
+        {
+            return objL.Listado_Todo_Emleado();
+        }
+
+
+    }
+}
---
+++ sol_ProyDSW_Cibertec2018/ProyDSW_Cibertec2018/Controllers/EmpleadoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using Dominio.MainModule;
+using Dominio.Core.Entidades;
+
+
+namespace ProyDSW_Cibertec2018.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+
+
+        EmpleadoManager em = new EmpleadoManager();
+
+
+        public ActionResult ListaTodoEmpleados()
+        {
+            return View(em.Listado_Todo_Emleado().ToList());
+        }
+
+        
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Empleado/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleado/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Empleado/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Empleado/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Empleado/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Empleado/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
