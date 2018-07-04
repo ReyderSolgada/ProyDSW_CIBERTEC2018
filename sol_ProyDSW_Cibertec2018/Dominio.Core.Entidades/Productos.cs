6333c9e3f45185f6e94f4ec23aeafd42948d1737
suite-sj202 <suite-sj202@SUITESJ202-ST03>
2018-07-03 20:39:13 -0500

    Cesar_clase_Producto

---
+++ sol_ProyDSW_Cibertec2018/Dominio.Core.Entidades/Productos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Core.Entidades
+{
+    class Productos
+    {
+
+        [DisplayName("Código de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Código Obligatorio")]
+        public int codpro { get; set; }
+        [DisplayName("Nombre de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Nombre Obligatorio")]
+        public String  nompro { get; set; }
+        [DisplayName("Stock de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Stock Obligatorio")]
+        public int stock { get; set; }
+        [DisplayName("Precio de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Precio Obligatorio")]
+        public double precio { get; set; }
+        [DisplayName("Tipo de Producto")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Tipo de producto obligatorio")]
+        public int codtipo { get; set; }
+        [DisplayName("estado")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Estado Obligatorio")]
+        public int eliminado { get; set; }
+
+    }
+}
